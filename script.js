eab696ab9486cbb0affdc8f611797312410596b7
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-08-20 17:08:54 -0400

    Updated port to work with Render

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('bookForm').addEventListener('click', function(event) {
     event.preventDefault();  // Prevent any default action
 
     // Redirect the browser to the Flask route that initiates the OAuth flow
-    window.location.href = 'https://localhost:5000/signin';
+    window.location.href = 'https://freebooks-b7fl.onrender.com/signin';
 });
